2cc1f61a357b0a642f734435a61ae12d87adaa93
reynanfausto <reynanfausto@gmail.com>
2015-01-30 17:02:58 +0800

    Modefied navigation.html and added js files

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,8 @@ jQuery(window).load(function () {
 	// will fade out the whole DIV that covers the website.
 	jQuery("#preloader").delay(1000).fadeOut("slow");
 })
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
